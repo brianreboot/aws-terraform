8d843e60bb9e19a8227c7fa516ce1849267de68e
Your Name <you@example.com>
2023-09-22 14:19:05 +0900

    added additional aws accounts

---
+++ awsaccount.tf
@@ -0,0 +1,9 @@
+variable "presales-aws" {
+  description = "use the AWS Presales Account"
+  default     = "presales-aws"
+}
+
+variable "coe-aws" {
+  description = "use the AWS COE Account"
+  default     = "coe-aws"
+}
