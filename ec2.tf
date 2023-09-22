ac85574c3ef87354cd17d21f224f64a57a1a552c
Your Name <you@example.com>
2023-09-22 13:30:59 +0900

    fix variables

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,14 @@
 resource "aws_instance" "ec2" {
     ami                                  = data.aws_ami.windows2022.id
+
     instance_type                        = var.t3_nano
     associate_public_ip_address          = true
     security_groups                      = []
     subnet_id                            = aws_subnet.terraform-subnet.id
     tags                                 = {
-        "Name" = var.project_tags+"-ec2"
+        "Name" = "{var.project_tags}-ec2"
     }
     tags_all                             = {
-        "Name" = var.project_tags+"-ec2"
+        "Name" = "{var.project_tags}-ec2"
     }
 }
