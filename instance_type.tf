94eb1eb6a678073e77dbb58822e458bf6caba020
Your Name <you@example.com>
2023-09-22 17:39:13 +0900

    fixed formatting

--- instance_type.tf
+++ instance_type.tf
@@ -1,14 +1,11 @@
 variable "t3_nano" {
-  description = "Type of EC2 instance to provision"
-  default     = "t3.nano"
+  default = "t3.nano"
 }
 
 variable "t3_micro" {
-  description = "Type of EC2 instance to provision"
-  default     = "t3.micro"
+  default = "t3.micro"
 }
 
 variable "t3_xlarge" {
-  description = "Type of EC2 instance to provision"
-  default     = "t3.xlarge"
+  default = "t3.xlarge"
 }
