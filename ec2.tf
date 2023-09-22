af54cc4f41ec7afaf809498de7ee519aede414b3
Your Name <you@example.com>
2023-09-22 13:12:12 +0900

    removed az

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2" {
     ami                                  = data.aws_ami.windows2022.id
-    availability_zone                    = "ap-northeast-3a"
+    # availability_zone                    = "ap-northeast-3a"
     instance_type                        = var.t3_nano
     associate_public_ip_address          = true
     security_groups                      = []
