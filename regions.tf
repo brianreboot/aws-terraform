2bbd290b3ba154754c093e40a3e3659358744898
Your Name <you@example.com>
2023-09-22 13:52:44 +0900

    added tokyo

--- regions.tf
+++ regions.tf
@@ -2,3 +2,8 @@ variable "osaka" {
   description = "Deploy to Osaka"
   default     = "ap-northeast-3"
 }
+
+variable "tokyo" {
+  description = "Deploy to Tokyo"
+  default     = "ap-northeast-1"
+}
