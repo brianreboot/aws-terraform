047537fd4f59da8004ca1063eb4c97cc9bf56407
Your Name <you@example.com>
2023-09-25 05:43:32 +0900

    updated configs

---
+++ TEMPLATE-VPC/instnace2.tf
@@ -0,0 +1,27 @@
+// Instance Type
+variable "ec2_instance" {
+  default = "t3.nano"
+}
+
+// Instance Name 
+variable "instance_tags" {
+  default = "instance2"
+}
+
+resource "aws_instance" "ec2" {
+    count                       = 1
+    ami                         = data.aws_ami.windows2022.id
+    instance_type               = var.ec2_instance
+    associate_public_ip_address = true
+    security_groups             = []
+    subnet_id                   = aws_subnet.terraform-subnet.id
+    
+    tags       = {
+        "Name" = var.instance_tags
+    }
+    
+    tags_all   = {
+        "Name" = var.instance_tags
+    }
+
+}
--- TEMPLATE-VPC/variable_instance_type.tf
+++
@@ -1,8 +0,0 @@
-# available variable types
-# t3.nano
-# t3.micro
-# t3.xlarge
-
-variable "ec2_instance" {
-  default = "t3.nano"
-}
